04d2a118055726b5809ee43f9fddb84c4618d42d
pramod <pramodmohanty033@gmail.com>
2022-04-30 11:06:01 +0530

    my first gitpush

---
+++ student-details_univeersity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace student_details_univeersity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
